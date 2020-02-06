2c829435d66d4e96a3cb6ed3e104f3921795c58a
DonWala <wala.radoslaw@gmail.com>
2020-02-06 23:08:43 +0100

    All fixed

--- src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
+++ src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
@@ -1,30 +1,44 @@
 package pl.edu.agh.mwo.invoice;
 
+
 import java.math.BigDecimal;
-import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 import pl.edu.agh.mwo.invoice.product.Product;
 
 public class Invoice {
-	private Collection<Product> products;
+	private Map<Product, Integer> products = new HashMap<Product, Integer>();
 
 	public void addProduct(Product product) {
-		// TODO: implement
+		addProduct(product, 1);
 	}
 
 	public void addProduct(Product product, Integer quantity) {
-		// TODO: implement
+		if (quantity == 0 || quantity < 0) throw new IllegalArgumentException();
+		products.put(product, quantity);
 	}
 
 	public BigDecimal getSubtotal() {
-		return null;
+		BigDecimal netPrice = BigDecimal.ZERO;
+
+		for (Product product : products.keySet()) {
+			Integer quantity = products.get(product);
+			netPrice = netPrice.add(product.getPrice().multiply(new BigDecimal(quantity)));
+		}
+		return netPrice;
 	}
 
 	public BigDecimal getTax() {
-		return null;
+		return getTotal().subtract(getSubtotal());
 	}
 
 	public BigDecimal getTotal() {
-		return null;
+		BigDecimal grossPrice = BigDecimal.ZERO;
+		for (Product product : products.keySet()) {
+			Integer quantity = products.get(product);
+			grossPrice = grossPrice.add(product.getPriceWithTax().multiply(new BigDecimal(quantity)));
+		}
+		return grossPrice;
 	}
 }
