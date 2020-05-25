644b843d45e246da2ec7baa45d4ed99b080cd5c2
DonWala <wala.radoslaw@gmail.com>
2020-05-25 23:20:03 +0200

    zaraz wybuchne

--- src/main/java/pl/edu/agh/mwo/invoice/product/Product.java
+++ src/main/java/pl/edu/agh/mwo/invoice/product/Product.java
@@ -1,46 +1,3 @@
-<<<<<<< HEAD
-package pl.edu.agh.mwo.invoice.product;
-
-import java.math.BigDecimal;
-
-public abstract class Product {
-    private final String name;
-
-    private final BigDecimal price;
-
-    private final BigDecimal taxPercent;
-
-    protected Product(String name, BigDecimal price, BigDecimal tax) {
-        if (name == null
-                || name.equals("")
-                || price == null
-                || tax == null
-                || tax.compareTo(new BigDecimal(0)) < 0
-                || price.compareTo(new BigDecimal(0)) < 0) {
-            throw new IllegalArgumentException();
-        }
-        this.name = name;
-        this.price = price;
-        this.taxPercent = tax;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public BigDecimal getPrice() {
-        return price;
-    }
-
-    public BigDecimal getTaxPercent() {
-        return taxPercent;
-    }
-
-    public BigDecimal getPriceWithTax() {
-        return price.multiply(taxPercent).add(price);
-    }
-}
-=======
 package pl.edu.agh.mwo.invoice.product;
 
 import java.math.BigDecimal;
@@ -82,4 +39,4 @@ public abstract class Product {
 		return this.price.add(this.price.multiply(this.taxPercent));
 	}
 }
->>>>>>> 2c829435d66d4e96a3cb6ed3e104f3921795c58a
+
